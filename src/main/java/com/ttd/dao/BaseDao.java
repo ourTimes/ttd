2dd44bf395264b9ed5a31747f6a7fede0918d621
hanxiaodong <mr.hanxd@163.com>
2014-10-20 17:06:36 +0800

    add genType to BaseDao , extract common methods

--- src/main/java/com/ttd/dao/BaseDao.java
+++ src/main/java/com/ttd/dao/BaseDao.java
@@ -1,5 +1,47 @@
 package com.ttd.dao;
 
-public class BaseDao {
+import java.io.Serializable;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public abstract class BaseDao<T> {
+	
+    @Autowired
+    private SessionFactory sessionFactory;
+
+    protected Class<T> entityClass;
+
+    public BaseDao() {
+        this.entityClass = getEntityClass();
+    }
+    
+    protected abstract Class<T> getEntityClass();
+    
+    public T load(Serializable id) {
+        if (null == id)return null;
+        return (T) getSession().load(entityClass, id);
+    }
+    
+    public T get(Serializable id){
+        if (null == id)
+            return null;
+        return (T) getSession().get(entityClass, id);
+    }
+    
+    public Serializable save(T entity) {
+    	if (null == entity)return null;
+        return getSession().save(entity);
+    }
+    
+    public void saveOrUpdate(T entity) {
+        if (null == entity)return;
+        getSession().saveOrUpdate(entity);
+    }
+
+    protected final Session getSession() {
+        return sessionFactory.getCurrentSession();
+    }
 
 }
--- src/main/java/com/ttd/dao/user/UserDao.java
+++ src/main/java/com/ttd/dao/user/UserDao.java
@@ -1,7 +1,13 @@
 package com.ttd.dao.user;
 
 import com.ttd.dao.BaseDao;
+import com.ttd.entity.User;
 
-public class UserDao extends BaseDao {
+public class UserDao extends BaseDao<User> {
+
+	@Override
+	protected Class<User> getEntityClass() {
+		return User.class;
+	}
 
 }
