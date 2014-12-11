5bae7aeb36e38e782ffe67d0aae778e8ef5b33d4
hanxiaodong <hanxiaodong@lightinthebox.com>
2014-12-11 19:24:23 +0800

    import modelAndAttribute

--- src/main/java/com/ttd/controller/MessageController.java
+++ src/main/java/com/ttd/controller/MessageController.java
@@ -6,6 +6,7 @@ import java.util.Date;
 import java.util.List;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
